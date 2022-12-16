5a4b13ce3d27bcae8b96befb03ac61e323fe7ac1
Kenneth Mensink <MENS411177@exactsoftware.com>
2022-12-16 16:12:06 +0100

    working on menu styling

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { SidebarModule } from './sidebar/sidebar.module';
+import { TopbarModule } from './topbar/topbar.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, SidebarModule],
+  imports: [CommonModule, RouterModule, SidebarModule, TopbarModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
