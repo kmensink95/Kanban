7a897f5ee3b5e63a8daaa683713033e1204519fe
Kenneth Mensink <MENS411177@exactsoftware.com>
2022-12-21 14:32:37 +0100

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { Tooltip2Component } from './tooltip2/tooltip2.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, Tooltip2Component],
   imports: [BrowserModule, AppRoutingModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
